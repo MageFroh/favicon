abd77d2329dea4e744349314e7401277ff9afa71
MageFroh <lecardob+github@gmail.com>
2013-08-25 21:15:24 +0100

    Use plugin hooks instead of updating every second

--- init.js
+++ init.js
@@ -1,18 +1,23 @@
 (function() {
-	var init = function()
-	{
-		console.log("Init favicon");
-		setInterval(checkTitle, 1000);
-	}
-
-  	var checkTitle = function()
-	{
+	var checkTitle = function() {
 		var count = document.title.match(/\((\d+)\)/);
 		
-		if(count)
+		if(count) {
+			console.log("Favicon: new unread count: " + count[1]);
 			favicon.badge(count[1]);
-		else
+		} else {
+			console.log("Favicon: no unread article");
 			favicon.badge();
+		}
+
+		return true;
+	}
+
+	var init = function() {
+		console.log("Favicon: init");
+                PluginHost.register(PluginHost.HOOK_RUNTIME_INFO_LOADED,  checkTitle);
+                PluginHost.register(PluginHost.HOOK_ARTICLE_RENDERED,     checkTitle);
+                PluginHost.register(PluginHost.HOOK_ARTICLE_RENDERED_CDM, checkTitle);
 	}
 
 	init();
